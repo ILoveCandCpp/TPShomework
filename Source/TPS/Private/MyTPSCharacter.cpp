1ae872a87560bf2920c91d9a3297fe4591233b1f
ZQRS-DESKCOMPUT\zqr98 <zqr980212@outlook.com>
2021-11-11 00:15:30 +0800

    add Gun

--- Source/TPS/Private/MyTPSCharacter.cpp
+++ Source/TPS/Private/MyTPSCharacter.cpp
@@ -3,12 +3,44 @@
 
 #include "MyTPSCharacter.h"
 
+#include "TPSProjectile.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 AMyTPSCharacter::AMyTPSCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//实例化你的类组件
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+
+	//设置角色网格体变换的位置和旋转
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
+
+	//将你的类组件附加到默认角色的骨骼网格体组件。
+
+	SpringArmComp->SetupAttachment(GetMesh());
+
+	CameraComp->SetupAttachment(SpringArmComp,USpringArmComponent::SocketName);
+
+	//设置弹簧臂的类变量
+
+	SpringArmComp->bUsePawnControlRotation = true;
+
+	//设置角色动作组件的类变量
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+
+	GetCharacterMovement()->bIgnoreBaseRotation = true;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +48,116 @@ void AMyTPSCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AMyTPSCharacter::MoveForward(float AxisValue)
+{
+	if ((Controller != nullptr) && (AxisValue != 0.0f))
+	{
+		// 找出向前方向
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// 获取向前矢量
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, AxisValue);
+	}
+}
+
+void AMyTPSCharacter::MoveRight(float AxisValue)
+{
+	if ((Controller != nullptr) && (AxisValue != 0.0f))
+	{
+		// 找出向右方向
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// 获取向右矢量 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// 添加该方向动作
+		AddMovementInput(Direction, AxisValue);
+	}
+}
+
+void AMyTPSCharacter::BeginSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 1000.0f;
+}
+
+void AMyTPSCharacter::EndSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = 600.f;
+}
+
+void AMyTPSCharacter::BeginCrouch()
+{
+	Crouch();
+}
+
+void AMyTPSCharacter::EndCrouch()
+{
+	UnCrouch();
+}
+
+void AMyTPSCharacter::Jump()
+{
+	bIsJumping = true;
+	Super::Jump();
+}
+
+void AMyTPSCharacter::StopJumping()
+{
+	Super::StopJumping();
+	bIsJumping = false;
+}
+
+void AMyTPSCharacter::Fire()
+{
+	// 试图发射发射物。
+	UE_LOG(LogTemp, Error, TEXT("FIRE"));
+	isFire = true;
+	if (ProjectileClass)
+	{
+		// 获取摄像机变换
+		FVector CameraLocation;
+		FRotator CameraRotation;
+		GetActorEyesViewPoint(CameraLocation, CameraRotation);
+		UE_LOG(LogTemp, Error, TEXT("FIRE2"));
+		// 设置MuzzleOffset，在略靠近摄像机前生成发射物。
+		MuzzleOffset.Set(100.0f, 0.0f, 0.0f);
+
+		// 将MuzzleOffset从摄像机空间变换到世界空间。
+		FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
+
+		// 使目标方向略向上倾斜。
+		FRotator MuzzleRotation = CameraRotation;
+		MuzzleRotation.Pitch += 10.0f;
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = GetInstigator();
+
+			// 在枪口位置生成发射物。
+			ATPSProjectile* Projectile = World->SpawnActor<ATPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+			if (Projectile)
+			{
+				// 设置发射物的初始轨迹。
+				FVector LaunchDirection = MuzzleRotation.Vector();
+				Projectile->FireInDirection(LaunchDirection);
+			}
+		}
+		// isFire = false;
+	}
+	
+}
+
+void AMyTPSCharacter::StopFire()
+{
+	isFire = false;
 }
 
 // Called every frame
@@ -30,5 +172,19 @@ void AMyTPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyTPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyTPSCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+
+	PlayerInputComponent->BindAction("Jump",IE_Pressed,this,&AMyTPSCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump",IE_Released,this,&AMyTPSCharacter::StopJumping);
+	PlayerInputComponent->BindAction("Crouch",IE_Pressed,this,&AMyTPSCharacter::BeginCrouch);
+	PlayerInputComponent->BindAction("Crouch",IE_Released,this,&AMyTPSCharacter::EndCrouch);
+	PlayerInputComponent->BindAction("Sprint",IE_Pressed,this,&AMyTPSCharacter::BeginSprint);
+	PlayerInputComponent->BindAction("Sprint",IE_Released,this,&AMyTPSCharacter::EndSprint);
+
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AMyTPSCharacter::Fire);
+	PlayerInputComponent->BindAction("Fire", IE_Released, this, &AMyTPSCharacter::StopFire);
 }
 
--- Source/TPS/Public/MyTPSCharacter.h
+++ Source/TPS/Public/MyTPSCharacter.h
@@ -15,9 +15,80 @@ public:
 	// Sets default values for this character's properties
 	AMyTPSCharacter();
 
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bIsJumping = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float Health = 1.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float Energy = 1.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	int32 Ammo = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	int32 OnceFiredAmmo = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	int32 MaxAmmo = 30;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	bool isFire = false;
+	// 枪口相对于摄像机位置的偏移。
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FVector MuzzleOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	bool bHasGun = true;
+
+	// 要生成的发射物类。
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Projectile)
+	TSubclassOf<class ATPSProjectile> ProjectileClass;
+	
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	//跟随玩家背后摄像机的弹簧臂组件
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class USpringArmComponent* SpringArmComp;
+
+	//玩家跟随摄像机
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UCameraComponent* CameraComp;
+	
+	//为向前/向后输入而调用
+
+	void MoveForward(float InputAxis);
+
+	//为左/右侧输入而调用
+
+	void MoveRight(float InputAxis);
+	
+	//将角色移动速度设置为冲刺值。
+	void BeginSprint();
+
+	//将角色移动速度设置回默认速度值。
+	void EndSprint();
+
+	//角色蹲伏请求
+	void BeginCrouch();
+
+	//角色结束蹲伏请求
+	void EndCrouch();
+
+	//角色跳
+	void Jump() override;
+
+	//角色停止跳
+	void StopJumping() override;
+
+	void Fire();
+
+	void StopFire();
 
 public:	
 	// Called every frame
